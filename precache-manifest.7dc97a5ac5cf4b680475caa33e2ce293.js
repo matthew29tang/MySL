2171bbef5aeb47c599dbb01e6962298a42d75c3b
matthew29tang <matthew29tang@gmail.com>
2021-04-26 15:30:43 -0700

    Updates

---
+++ precache-manifest.7dc97a5ac5cf4b680475caa33e2ce293.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd922063f150a326c8b1d3d0a5336b6f",
+    "url": "/MySL/index.html"
+  },
+  {
+    "revision": "4a914e73be87931ce38e",
+    "url": "/MySL/static/css/main.fbd09caa.chunk.css"
+  },
+  {
+    "revision": "7e3ccd9f7df4c38bdd89",
+    "url": "/MySL/static/js/2.c48402f5.chunk.js"
+  },
+  {
+    "revision": "4a914e73be87931ce38e",
+    "url": "/MySL/static/js/main.d887ea06.chunk.js"
+  },
+  {
+    "revision": "a833805829937b636b9d",
+    "url": "/MySL/static/js/runtime~main.fff91dcd.js"
+  },
+  {
+    "revision": "3f7e59ccba886ccd5a4c11fb4c47a5e8",
+    "url": "/MySL/static/media/dropzone.3f7e59cc.svg"
+  },
+  {
+    "revision": "bcc693c2b965e29263e8de8f692c0176",
+    "url": "/MySL/static/media/is.bcc693c2.png"
+  },
+  {
+    "revision": "dc80d2f25e5d3cd18905c6ef1913ce44",
+    "url": "/MySL/static/media/my.dc80d2f2.png"
+  },
+  {
+    "revision": "bb0534c8267fd676108f520f60d1d92f",
+    "url": "/MySL/static/media/progress.bb0534c8.svg"
+  }
+]);
